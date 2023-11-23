70f5a0a644d45bbecfec226d6821937425b21d81
Leo <151764103+MonkeyDeveloper05@users.noreply.github.com>
2023-11-23 23:16:32 +0100

    Add files via upload

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="flex items-center justify-around h-16 text-5xl text-white border-b shadow-md bg-slate-500 border-slate-300"> 
+        <h1>Ciao</h1>
+        
+        <h2>hello</h2>
+    </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  type ToDo = {
+    id: number,
+    title: string,
+    completed: boolean,
+  } 
+
+  const list: ToDo[] = [
+    {
+      id: 1,
+      title: "ciao",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "hello",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "hola",
+      completed: true,
+    }
+]
+</script>
+
+<template>
+  <main class="container mt-4">
+    <li v-for="item in list" :key="item.id">
+      {{ item.title }}
+    </li>
+  </main>
+</template>
